f100fff5c564930593e7a29a6c179c042d0ec0b5
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2024-02-04 21:48:14 -0500

    refactor:apaga pastas que nao usei

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,6 @@ import PetsList from '../views/Eliana/PetsList.vue'
 import PetsPesquisa from '../views/Eliana/PetsPesquisa.vue'
 import Main from '../views/Main.vue'
 import PetProfile from '../views/Eliana/PetProfile.vue'
-import Home1 from '../views/pessoa1/Home.vue'
-import Home2 from '../views/pessoa2/Home.vue'
-import Home3 from '../views/pessoa3/Home.vue'
-import Home4 from '../views/pessoa4/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -37,27 +33,7 @@ const router = createRouter({
       path: '/main',
       name: 'main',
       component: Main
-    },
-    {
-      path: '/home1',
-      name: 'home1',
-      component: Home1
-    },
-    {
-      path: '/home2',
-      name: 'home2',
-      component: Home2
-    },
-    {
-      path: '/home3',
-      name: 'home3',
-      component: Home3
-    },
-    {
-      path: '/home4',
-      name: 'home4',
-      component: Home4
-    },
+    },  
    
   ]
 })
