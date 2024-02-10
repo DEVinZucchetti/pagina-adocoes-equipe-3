6ae0a92f413389e7557e533739962c8ff78fdf92
Guilherme <guiduartesbs@hotmail.com>
2024-02-10 16:50:36 -0300

    fix: corrige nome da pasta

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Main from '../views/Main.vue'
-import Home1 from '../views/pessoa1/Home.vue'
+import Home1 from '../views/Guilherme/Home.vue'
 import Home2 from '../views/pessoa2/Home.vue'
 import Home3 from '../views/pessoa3/Home.vue'
 import Home4 from '../views/pessoa4/Home.vue'
-import PetsList from '../views/pessoa1/Pets.vue'
-import PetsProfile from '../views/pessoa1/Profile.vue'
+import PetsList from '../views/Guilherme/Pets.vue'
+import PetsProfile from '../views/Guilherme/Profile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
