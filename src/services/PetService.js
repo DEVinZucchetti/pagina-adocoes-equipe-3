a8dda1874ef007420c58f591bf774f663d0b9d3f
Guilherme <guiduartesbs@hotmail.com>
2024-02-10 22:22:47 -0300

    feat: add PetService

---
+++ src/services/PetService.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+class PetService{
+    async getAllPets(){
+        const response = await axios.get("http://localhost:8000/api/pets/adocao")
+        return response.data
+    }
+    async getOnePet(id){
+        const response = await axios.get(`http://localhost:8000/api/pets/${id}`)
+        return response.data
+    }
+    async adoptPet(data){
+        const response = await axios.post(`http://localhost:8000/api/pets/adocao`,data)
+        return response.data
+    }
+}
+export default new PetService
